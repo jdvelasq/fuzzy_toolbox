f6fc70bf91f1a6a1ab9efe6048119ed22e520e5c
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-06-04 18:43:42 -0500

    Update variable.py

--- pyfuzzy/variable.py
+++ pyfuzzy/variable.py
@@ -127,7 +127,6 @@ class FuzzyVariable:
         plt.gca().spines["bottom"].set_color("gray")
         plt.gca().spines["top"].set_visible(False)
         plt.gca().spines["right"].set_visible(False)
-        plt.close()
 
     def membership(self, value, fuzzyset):
         """Computes the valor of the membership function on a specifyied point of the universe for the fuzzy set.
